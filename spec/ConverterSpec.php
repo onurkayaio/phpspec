3d33cb0cd5b843791417c335919845491df68d83
onur kaya <onurkaya244@gmail.com>
2018-02-06 15:10:50 +0300

    Update ConverterSpec.php

--- spec/ConverterSpec.php
+++ spec/ConverterSpec.php
@@ -16,4 +16,12 @@ class ConverterSpec extends ObjectBehavior
     function it_returns_ten(){
         $this->convert('X')->shouldBe(10);
     }
+    
+    function it_returns_fifty(){
+        $this->convert('L')->shouldBe(50);
+    }
+    
+    function it_returns_hundred(){
+        $this->convert('C')->shouldBe(100);
+    }
 }
